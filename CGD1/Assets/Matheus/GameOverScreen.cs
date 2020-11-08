aa436e807aba1f97785aa9e9a4e1332d1f941707
ewhite755 <54605007+ewhite755@users.noreply.github.com>
2020-11-08 17:59:53 -0500

    changes in code

--- CGD1/Assets/Matheus/GameOverScreen.cs
+++ CGD1/Assets/Matheus/GameOverScreen.cs
@@ -18,4 +18,8 @@ public class GameOverScreen : MonoBehaviour
     {
         SceneManager.LoadScene("KitchenLevel");
     }
+    public void SewerLevel()
+    {
+        SceneManager.LoadScene("SewerLevel");
+    }
 }
